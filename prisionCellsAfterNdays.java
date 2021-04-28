0a2dca983203892b4f7ae3d4bf4a6cd2b8960b7b
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-04-28 12:36:20 +0530

    Add files via upload

--- prisionCellsAfterNdays.java
+++ prisionCellsAfterNdays.java
@@ -31,3 +31,60 @@ class Solution {
         return nums;
     }
 }
+////////////////////////////////////////////////
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        HashSet<String> h=new HashSet<>();
+        boolean isCycle=false;
+        int cycle=0;
+        
+        for(int i=1;i<=n;i++)
+        {
+          int[] temp=nextDay(cells,n);
+            String str=Arrays.toString(temp);
+            if(!h.contains(str))
+            {
+                cycle++;
+                h.add(str);
+            }
+            else
+            {
+                isCycle=true;
+                break;
+            }
+            cells=temp;
+        }
+            if(isCycle)
+            {
+                n=n%cycle;
+                for(int i=1;i<=n;i++)
+                {
+                   cells=nextDay(cells,n);
+                    
+                }
+                
+            }
+            
+        
+        return cells;
+        
+        
+        
+        
+        
+        
+        
+    }
+    public int[] nextDay(int nums[],int n)
+    {
+        int arr[]=new int[8];
+           for(int i=1;i<8-1;i++)
+           {
+               if(nums[i-1]==nums[i+1])
+                   arr[i]=1;
+               else 
+                   arr[i]=0;
+           }
+        return arr;
+    }
+}
